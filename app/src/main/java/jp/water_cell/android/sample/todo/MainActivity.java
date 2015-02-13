d306f2b0e2e91a08d3960145ce556f586610b511
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2015-02-14 02:34:53 +0900

    Add 'add' button & mock result

--- app/src/main/java/jp/water_cell/android/sample/todo/MainActivity.java
+++ app/src/main/java/jp/water_cell/android/sample/todo/MainActivity.java
@@ -1,8 +1,11 @@
 package jp.water_cell.android.sample.todo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,6 +17,7 @@ import butterknife.InjectView;
 public class MainActivity extends ActionBarActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_ADD = 0;
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
@@ -59,4 +63,33 @@ public class MainActivity extends ActionBarActivity {
         listView.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.add, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_add:
+                startActivityForResult(new Intent(MainActivity.this, TodoAddActivity.class), REQUEST_CODE_ADD);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_ADD) {
+            final Todo newTodo = data.getParcelableExtra(TodoAddActivity.RESULT_CODE_TODO);
+            adapter.add(newTodo);
+        }
+    }
 }
---
+++ app/src/main/java/jp/water_cell/android/sample/todo/TodoAddActivity.java
@@ -0,0 +1,61 @@
+package jp.water_cell.android.sample.todo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class TodoAddActivity extends ActionBarActivity {
+
+    public static final String RESULT_CODE_TODO = TodoAddActivity.class.getCanonicalName() + ".result_todo";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Button button = new Button(this);
+        button.setText("push");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random rand = new Random();
+                long id = rand.nextLong();
+
+                Todo todo = new Todo.Builder()
+                        .id(id)
+                        .title("タイトル" + id)
+                        .description("なかみ" + id)
+                        .timestamp(1234567890)
+                        .done(true)
+                        .build();
+
+                Intent intent = new Intent();
+                intent.putExtra(RESULT_CODE_TODO, todo);
+
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        setContentView(button);
+
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
